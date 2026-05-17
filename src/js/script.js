703a57d256d971f3457f08c6192f7756cb796c6d
vestFacil <lizggalmeida@gmail.com>
2026-05-17 15:23:40 -0300

    feat: paginas do topo

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+document.getElementById("quest").addEventListener("click", () => {
+    window.location.href = "questao.html";
+});
+
+document.getElementById("red").addEventListener("click", () => {
+    window.location.href = "redacao.html";
+});
+
+document.getElementById("caderno").addEventListener("click", () => {
+    window.location.href = "caderno.html";
+});
